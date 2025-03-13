999b848cbccec3bd6cf155de039bfb3debb9edcd
Braian-Zignago <braianzig77@gmail.com>
2025-03-12 22:37:25 -0300

    00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Braian-Zignago.py
@@ -0,0 +1,17 @@
+# Sitio oficial de Python: https://www.python.org/
+
+
+""" Comentario de varias
+lineas
+en Python con comillas dobles. """
+
+ID = 1212121
+name = 'Braian'
+edad = 22
+mayor_edad = True
+
+altura = 1.70
+peso = 85.5
+imc = peso / (altura * altura)
+
+print("Hola, Python!")
